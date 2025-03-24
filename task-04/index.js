54307cad17f01cf8f0e28b88c5da80ea960b2b48
Razvie-arr <vladikbyastinov@gmail.com>
2025-03-24 15:52:27 +0100

    task-04 done

--- task-04/index.js
+++ task-04/index.js
@@ -31,28 +31,16 @@ app.get('/', async (c) => {
     return c.html(rendered);
 });
 
-app.post('/todos', async (c) => {
-    const form = await c.req.formData();
-
-    todos.push({
-        id: todos.length + 1,
-        title: form.get('title'),
-        done: false,
-    });
-
-    return c.redirect('/');
-});
-
 app.get('/todos/:id/toggle', async (c) => {
     const id = Number(c.req.param('id'));
 
-    const todo = todos.find((todo) => todo.id === id);
+    const todo = await findTodoById(id);
 
     if (!todo) return c.notFound();
 
     todo.done = !todo.done;
 
-    return c.redirect('/');
+    return c.redirect(await getReferrer(c));
 });
 
 app.get('/todos/:id/remove', async (c) => {
@@ -67,6 +55,55 @@ app.get('/todos/:id/remove', async (c) => {
     return c.redirect('/');
 });
 
+app.get('/todos/:id', async (c) => {
+    const id = Number(c.req.param('id'));
+    const todo = await findTodoById(id);
+
+    if (!todo) return c.notFound();
+
+    const rendered = await renderFile('views/todo_item.html', {
+        todo,
+    });
+
+    return c.html(rendered);
+});
+
+app.post('/todos', async (c) => {
+    const form = await c.req.formData();
+
+    todos.push({
+        id: todos.length + 1,
+        title: form.get('title'),
+        done: false,
+    });
+
+    return c.redirect('/');
+});
+
+app.post('/todos/:id/edit', async (c) => {
+    const id = Number(c.req.param('id'));
+    const todo = await findTodoById(id);
+
+    if (!todo) return c.notFound();
+
+    const form = await c.req.formData();
+    const newTitle = form.get('newTitle');
+
+    if (!newTitle) return (c.statusCode = 400);
+
+    todo.title = newTitle;
+
+    return c.redirect(await getReferrer(c));
+});
+
 serve(app, (info) => {
     console.log('App started on http://localhost:' + info.port);
 });
+
+const findTodoById = async (id) => {
+    return todos.find((todo) => todo.id === id);
+};
+
+const getReferrer = async (c) => {
+    return c.req.header('Referer') || '/';
+};
